b3f3a788304fa2bbbbfb064afcec385da376d5ff
TomBooth <thomas.alexander.booth@gmail.com>
2022-07-28 00:09:35 +0100

    login button added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import TodoList from './components/TodoList';
+import { Auth0Provider } from "@auth0/auth0-react";
+import LoginButton from './components/LoginButton';
+
 
 function App() {
   return (
     <div className="todo-app">
+     <Auth0Provider
+     domain="dev-m2mklypb.eu.auth0.com"
+    clientId="GgU70co849jx0VxY2ZLspyblOfU85AIc"
+    redirectUri={window.location.origin}
+  >
      <TodoList />
+     <LoginButton/>
+     </Auth0Provider>
     </div>
   );
 }
